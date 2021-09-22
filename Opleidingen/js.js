5c730ef7c8c0fbb99d3b970cf58061afc2d3c4d3
97067883Ludo <97067883@st.deltion.nl>
2021-09-22 08:44:59 +0200

    added code and finished Opleidingen

---
+++ Opleidingen/js.js
@@ -0,0 +1,37 @@
+let showCarmel = 0;
+let showDeltion1 = 0;
+let showDeltion2 = 0;
+
+function carmel(){
+    if(showCarmel == 0){
+        document.getElementById("carmel").style.display = "none";
+        showCarmel = 1;
+    }
+    else{
+        document.getElementById("carmel").style.display = "block";
+        showCarmel = 0;
+
+    }
+}
+function deltion1(){
+    if(showDeltion1 == 0){
+        document.getElementById("deltion1").style.display = "none";
+        showDeltion1 = 1;
+    }
+    else{
+        document.getElementById("deltion1").style.display = "block";
+        showDeltion1 = 0;
+    }
+
+}
+function deltion2(){
+    if(showDeltion2 ==0){
+        document.getElementById("deltion2").style.display = "none";
+        showDeltion2 = 1;
+    }
+    else{
+        document.getElementById("deltion2").style.display = "block";
+        showDeltion2 = 0;
+    }
+
+}

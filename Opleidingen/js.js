25e7f66b1bec6105130c8b4cc486df7a1499ff60
97067883Ludo <97067883@st.deltion.nl>
2021-09-23 11:09:59 +0200

    added final code

--- Opleidingen/js.js
+++ Opleidingen/js.js
@@ -4,33 +4,33 @@ let showDeltion2 = 0;
 
 function carmel(){
     if(showCarmel == 0){
-        document.getElementById("carmel").style.display = "none";
+        document.getElementById("carmel").style.display = "block";
         showCarmel = 1;
     }
     else{
-        document.getElementById("carmel").style.display = "block";
+        document.getElementById("carmel").style.display = "none";
         showCarmel = 0;
 
     }
 }
 function deltion1(){
     if(showDeltion1 == 0){
-        document.getElementById("deltion1").style.display = "none";
+        document.getElementById("deltion1").style.display = "block";
         showDeltion1 = 1;
     }
     else{
-        document.getElementById("deltion1").style.display = "block";
+        document.getElementById("deltion1").style.display = "none";
         showDeltion1 = 0;
     }
 
 }
 function deltion2(){
     if(showDeltion2 ==0){
-        document.getElementById("deltion2").style.display = "none";
+        document.getElementById("deltion2").style.display = "block";
         showDeltion2 = 1;
     }
     else{
-        document.getElementById("deltion2").style.display = "block";
+        document.getElementById("deltion2").style.display = "none";
         showDeltion2 = 0;
     }
 
